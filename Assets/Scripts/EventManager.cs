41706ac8fae6cc217a76cce35a2d4b2ae9adefe4
Sarah <phoesarah@hotmail.com>
2018-07-24 21:44:49 -0700

    created eventManager and Event

---
+++ Assets/Scripts/EventManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour {
+    private List<Event> eventlist;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void addEventtoList(Event ev)
+    {
+        eventlist.Add(ev);
+    }
+   
+    public List<Event> GetLog()
+    {
+        return eventlist;
+    }
+}
